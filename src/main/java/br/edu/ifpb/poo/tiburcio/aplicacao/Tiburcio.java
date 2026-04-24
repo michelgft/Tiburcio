f1900c20ec3f607a1981b2ad60f197ed287762d2
michelgft <michel.graffit@gmail.com>
2026-04-24 12:49:51 -0300

    adiciona estrutura de pacotes aplicacao, modelo e util

--- src/main/java/br/edu/ifpb/poo/tiburcio/aplicacao/Tiburcio.java
+++ src/main/java/br/edu/ifpb/poo/tiburcio/aplicacao/Tiburcio.java
@@ -1,6 +1,6 @@
-package br.edu.ifpb.poo.tiburcio;
+package br.edu.ifpb.poo.tiburcio.aplicacao;
 
-public class Main {
+public class Tiburcio {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/br/edu/ifpb/poo/tiburcio/modelo/Revista.java
@@ -0,0 +1,5 @@
+package br.edu.ifpb.poo.tiburcio.modelo;
+
+public class Revista {
+
+}
