ddd90b19b82aaf07f17ba72e9570369ecf9641f0
michelgft <michel.graffit@gmail.com>
2026-06-21 11:22:09 -0300

    implementa Menu e TiburcioUI (interface principal do sistema)

--- src/main/java/br/edu/ifpb/poo/tiburcio/ui/Menu.java
+++ src/main/java/br/edu/ifpb/poo/tiburcio/ui/Menu.java
@@ -1,5 +1,71 @@
 package br.edu.ifpb.poo.tiburcio.ui;
 
-public class Menu {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Menu {
+    private String titulo;
+    private String[] opcoes;
+    private String prompt;
+    private Console console;
+
+    public Menu exibir() {
+        final char TL = '╔';
+        final char TR = '╗';
+        final char BL = '╚';
+        final char BR = '╝';
+        final char HL = '═';
+        final char VL = '║';
+
+        int maxConteudo = titulo.length();
+        for (int i = 0; i < opcoes.length; i++) {
+            int tamanhoItem = (i + 1 + ") " + opcoes[i]).length();
+            if (tamanhoItem > maxConteudo) {
+                maxConteudo = tamanhoItem;
+            }
+        }
+
+        int largura = maxConteudo + 2;
+
+        System.out.print(TL);
+        for (int i = 0; i < largura; i++) System.out.print(HL);
+        System.out.println(TR);
+
+        int espacos = largura - titulo.length();
+        int esquerda = espacos / 2;
+        int direita = espacos - esquerda;
+
+        System.out.print(VL);
+        for (int i = 0; i < esquerda; i++) System.out.print(" ");
+        System.out.print(titulo);
+        for (int i = 0; i < direita; i++) System.out.print(" ");
+        System.out.println(VL);
+
+        System.out.print(VL);
+        for (int i = 0; i < largura; i++) System.out.print(HL);
+        System.out.println(VL);
+
+        for (int i = 0; i < opcoes.length; i++) {
+            String linha = (i + 1) + ") " + opcoes[i];
+            System.out.print(VL);
+            System.out.print(linha);
+            for (int j = linha.length(); j < largura; j++) System.out.print(" ");
+            System.out.println(VL);
+        }
+
+        System.out.print(BL);
+        for (int i = 0; i < largura; i++) System.out.print(HL);
+        System.out.println(BR);
+
+        return this;
+    }
+
+    public int leiaOpcao() {
+        System.out.print(Cores.AZUL + this.prompt + Cores.RESET);
+        return console.nextInt();
+    }
 }
--- src/main/java/br/edu/ifpb/poo/tiburcio/ui/TiburcioUI.java
+++ src/main/java/br/edu/ifpb/poo/tiburcio/ui/TiburcioUI.java
@@ -1,5 +1,90 @@
 package br.edu.ifpb.poo.tiburcio.ui;
 
-public class TiburcioUI {
+import br.edu.ifpb.poo.tiburcio.modelo.*;
 
+public class TiburcioUI {
+    
+    private Console console;
+    private EditoraUI editoraUI;
+    private ItemUI itemUI;
+    private JogoUI jogoUI;
+    private UsuarioUI usuarioUI;
+    private EmprestimoUI emprestimoUI;
+    
+    public TiburcioUI() {
+        this.console = new Console();
+        this.editoraUI = new EditoraUI(console);
+        this.itemUI = new ItemUI(console);
+        this.jogoUI = new JogoUI(console);
+        this.usuarioUI = new UsuarioUI(console);
+        this.emprestimoUI = new EmprestimoUI(console);
+    }
+    
+    public Console getConsole() {
+        return console;
+    }
+    
+    public EditoraUI getEditoraUI() {
+        return editoraUI;
+    }
+    
+    public ItemUI getItemUI() {
+        return itemUI;
+    }
+    
+    public JogoUI getJogoUI() {
+        return jogoUI;
+    }
+    
+    public UsuarioUI getUsuarioUI() {
+        return usuarioUI;
+    }
+    
+    public EmprestimoUI getEmprestimoUI() {
+        return emprestimoUI;
+    }
+    
+    public void exibirMenuPrincipal() {
+        console.clrscr();
+        Menu menu = new Menu(
+            "TIBURCIO - BIBLIOTECA",
+            new String[]{
+                "Gerenciar Editoras",
+                "Gerenciar Itens",
+                "Gerenciar Jogos",
+                "Gerenciar Usuários",
+                "Empréstimo/Devolução",
+                "Vendas",
+                "Consultas",
+                "Sair"
+            },
+            "Digite sua opção: ",
+            console
+        );
+        menu.exibir();
+    }
+    
+    public int lerOpcao() {
+        return console.nextInt();
+    }
+    
+    public void exibirMensagemSucesso(String mensagem) {
+        console.println(Cores.VERDE, mensagem);
+    }
+    
+    public void exibirMensagemErro(String mensagem) {
+        console.println(Cores.VERMELHO, mensagem);
+    }
+    
+    public void exibirMensagemInfo(String mensagem) {
+        console.println(Cores.AZUL, mensagem);
+    }
+    
+    public void limparTela() {
+        console.clrscr();
+    }
+    
+    public void pausar() {
+        console.pause();
+    }
 }
